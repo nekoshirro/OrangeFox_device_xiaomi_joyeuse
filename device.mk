69b424f0752325e28ad53b7afaaf443a581f703e
Darth9 <reyprimerjefe@gmail.com>
2022-07-14 21:58:25 +0700

    joyeuse: Adapt to OrangeFox Recovery
    Signed-off-by: nekoshirro <ais.muzakky@gmail.com>

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Fscrypt policy
+TW_USE_FSCRYPT_POLICY := 1
+
 # Decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
@@ -25,4 +28,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/commonsys-intf/display
+
+# Fastbootd stuff
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Exclude Apex
+TW_EXCLUDE_APEX := true
+
+# OEM otacert
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    vendor/recovery/security/miui
+
+# Libraries
+TARGET_RECOVERY_DEVICE_MODULES += libion vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
