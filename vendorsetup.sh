1df65b2da3aa35b1e35a30f2c32dcef87899e2af
Hafidz Muzakky <ais.muzakky@gmail.com>
2022-07-14 23:22:39 +0700

    joyeuse: Remove unsupported devices
    * This build only for joyeuse

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -38,7 +38,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
  	export ALLOW_MISSING_DEPENDENCIES=true
-	export TARGET_DEVICE_ALT="curtana,joyeuse,gram,excalibur"
+	export TARGET_DEVICE_ALT="joyeuse"
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 	export OF_USE_GREEN_LED=0
